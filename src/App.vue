e3673289e6c9623f0f22d3fa8f6b0691982cf92d
NeilWatts240 <vasilenko.m96@gmail.com>
2023-05-11 06:36:28 +0300

    qwe

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="app">
-    <post-form />
-    <post-list :posts="posts" />
+    <h1>Страница с постами</h1>
+    <my-button @click="showDialog" style="margin: 15px">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible"
+      ><post-form @create="createPost"
+    /></my-dialog>
+    <post-list :posts="posts" @remove="removePost" v-if="!isPostLoading" />
+    <div v-else>Идёт загрузка...</div>
   </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
+import axios from "axios";
 
 export default {
   components: { PostForm, PostList },
   data() {
     return {
-      posts: [
-        { id: 1, title: "JavaScript", body: "Описание поста" },
-        { id: 1, title: "JavaScript", body: "Описание поста" },
-        { id: 1, title: "JavaScript", body: "Описание поста" },
-      ],
-      title: "",
-      body: "",
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
     };
   },
   methods: {
-    createPost() {
-      const newPost = { id: Date.now(), title: this.title, body: this.body };
-      this.posts.push(newPost);
-      this.title = "";
-      this.body = "";
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
     },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostLoading = true;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert("Ошибка");
+      } finally {
+        this.isPostLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
   },
 };
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="post" v-for="post in posts">
-    <div><strong>Название:</strong>{{ post.title }}</div>
-    <div><strong>Описание:</strong>{{ post.body }}</div>
+  <div v-if="posts.length > 0">
+    <h3>Список пользователей</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    />
   </div>
+  <h2 v-else style="color: red">Список постов пуст</h2>
 </template>
 
 <script>
-export default { props: { posts: { type: Array, required: true } } };
+import PostItem from "@/components/PostItem.vue";
+
+export default {
+  components: { PostItem },
+  props: { posts: { type: Array, required: true } },
+};
 </script>
 
-<style scoped>
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-</style>
+<style scoped></style>
